52e266536ff8915f133dc1bfa97ee7f876a8a384
Daniil Kagan <dan.kagan@yandex.by>
2021-07-23 03:02:49 +0300

    refactoring

--- src/main/java/com/example/coffeehouse/repositories/CoffeeCostsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.coffeehouse.repositories;
-
-import com.example.coffeehouse.models.Coffee;
-import com.example.coffeehouse.models.converters.CoffeeCosts;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoffeeCostsRepository extends CrudRepository<CoffeeCosts, Integer> {
-}
